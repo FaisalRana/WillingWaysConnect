b3a0e4865c17b7c08b4d46af987e7d40253bd349
lasifanar <inquisitive@gmail.com>
2024-08-17 16:18:43 +0500

    added left sidebar to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Home from "./pages/home";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/login" element={<Login />} exact />
-        <Route path="/profile" element={<Profile />} exact />
-        <Route path="/" element={<Home />} exact />
+        <Route element={<LoggedInRoutes />}>
+          {/* Thid is a protected route.  Profile and home are nested within loggedinroutes.  Loggedinroutes checks if a user is logged in.  If the user is logged in then it returns the outlet which allows for the child routes to render.  if no user exists it renders the login page instead.
+           */}
+          <Route path="/profile" element={<Profile />} exact />
+          <Route path="/" element={<Home />} exact />
+        </Route>
+          {/* When a user tries to access /login.  not logged in routes does a check to see if the user is logged in or not.  only if the user is not logged in does it allow the login page. 
+           */}
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" element={<Login />} exact />
+        </Route>
       </Routes>
     </div>
   );
