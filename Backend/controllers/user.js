3fda1a75ce44399f32035094e3ed5ae3c2a42445
lasifanar <inquisitive@gmail.com>
2024-08-26 19:19:29 +0500

    added create password and code verification backend and frontend code

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -4,10 +4,12 @@ const {
   validateUsername,
 } = require("../helpers/validation");
 const User = require("../models/User");
+const Code = require("../models/Code");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../helpers/tokens");
-const { sendVerificationEmail } = require("../helpers/mailer");
+const { sendVerificationEmail, sendResetCode } = require("../helpers/mailer");
+const generateCode = require("../helpers/generateCode");
 exports.register = async (req, res) => {
   try {
     const {
@@ -93,16 +95,16 @@ exports.activateAccount = async (req, res) => {
     const { token } = req.body;
     const user = jwt.verify(token, process.env.TOKEN_SECRET);
     const check = await User.findById(user.id);
+
     if (validUser !== user.id) {
       return res.status(400).json({
-        message:
-          "Activation failed: You don't have the authorization to complete this operation.",
+        message: "You don't have the authorization to complete this operation.",
       });
     }
     if (check.verified == true) {
       return res
         .status(400)
-        .json({ message: "This email is already activated" });
+        .json({ message: "This email is already activated." });
     } else {
       await User.findByIdAndUpdate(user.id, { verified: true });
       return res
@@ -143,15 +145,14 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-
 exports.sendVerification = async (req, res) => {
   try {
     const id = req.user.id;
     const user = await User.findById(id);
     if (user.verified === true) {
-      return res
-        .status(400)
-        .json({ message: "This email is already activated" });
+      return res.status(400).json({
+        message: "This account is already activated.",
+      });
     }
     const emailVerificationToken = generateToken(
       { id: user._id.toString() },
@@ -159,9 +160,75 @@ exports.sendVerification = async (req, res) => {
     );
     const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
     sendVerificationEmail(user.email, user.first_name, url);
+    return res.status(200).json({
+      message: "Email verification link has been sent to your email.",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.findUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email }).select("-password");
+    if (!user) {
+      return res.status(400).json({
+        message: "Account does not exists.",
+      });
+    }
+    return res.status(200).json({
+      email: user.email,
+      picture: user.picture,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-    return res
-      .status(200)
-      .json({ message: "Email verification link has been sent to your email" });
-  } catch (error) {}
+exports.sendResetPasswordCode = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email }).select("-password");
+    await Code.findOneAndRemove({ user: user._id });
+    const code = generateCode(5);
+    const savedCode = await new Code({
+      code,
+      user: user._id,
+    }).save();
+    sendResetCode(user.email, user.first_name, code);
+    return res.status(200).json({
+      message: "Email reset code has been sent to your email",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.validateResetCode = async (req, res) => {
+  try {
+    const { email, code } = req.body;
+    const user = await User.findOne({ email });
+    const Dbcode = await Code.findOne({ user: user._id });
+    if (Dbcode.code !== code) {
+      return res.status(400).json({
+        message: "Verification code is wrong..",
+      });
+    }
+    return res.status(200).json({ message: "ok" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.changePassword = async (req, res) => {
+  const { email, password } = req.body;
+
+  const cryptedPassword = await bcrypt.hash(password, 12);
+  await User.findOneAndUpdate(
+    { email },
+    {
+      password: cryptedPassword,
+    }
+  );
+  return res.status(200).json({ message: "ok" });
 };
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -3,9 +3,14 @@ const {
   register,
   activateAccount,
   login,
+  auth,
   sendVerification,
+  findUser,
+  sendResetPasswordCode,
+  validateResetCode,
+  changePassword,
 } = require("../controllers/user");
-const { authUser } = require("../middlewares/auth");
+const { authUser } = require("../middlwares/auth");
 
 const router = express.Router();
 
@@ -13,5 +18,9 @@ router.post("/register", register);
 router.post("/activate", authUser, activateAccount);
 router.post("/login", login);
 router.post("/sendVerification", authUser, sendVerification);
+router.post("/findUser", findUser);
+router.post("/sendResetPasswordCode", sendResetPasswordCode);
+router.post("/validateResetCode", validateResetCode);
+router.post("/changePassword", changePassword);
 
 module.exports = router;
